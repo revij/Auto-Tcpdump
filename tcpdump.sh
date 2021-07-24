ef8a40d7a6890193e1e350494d1a001ca07c1838
Ioannis Karamperis <40238062+revij@users.noreply.github.com>
2021-07-24 22:03:46 +0200

    Adjust variables and retrieve interface automatically

--- tcpdump.sh
+++ tcpdump.sh
@@ -1,8 +1,10 @@
 clear
 echo -e "\033[32;5mAuto Tcpdump is now running \033[0m"
-interface=eth0 ## change this
+interface=`route | grep '^default' | grep -o '[^ ]*$'`
 dumpdir=/root/tcpdump
-capturefile=/root/tcpdump/dogeiana.txt
+capturedir=/root/tcpdump/dumps
+triggerpackets=204800
+capturesize=8600
 
 while /bin/true; do
     pkt_old=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
@@ -10,12 +12,12 @@ while /bin/true; do
     pkt_new=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
     pkt=$(( $pkt_new - $pkt_old ))
     echo -ne "\r$pkt Packets/Sec\033[0K"
-    if [ $pkt -gt 1024 ]; then
+    if [ $pkt -gt $triggerpackets ]; then
          echo "Under Attack! Dumping Packets."
-         tcpdump -n -s0 -c 8600 -w $dumpdir/DUMP.`date +"%Y%m%d-%H%M%S"`.cap
-         tshark -i - < "$dumpdir/dump.`date +"%H%M"`.cap" > "$capturefile"
+         tcpdump -n -s0 -c $capturesize -w $dumpdir/DUMP.`date +"%Y%m%d-%H%M"`.cap
+         tshark -i - < "$dumpdir/dump.`date +"%Y%m%d-%H%M"`.cap" > $capturedir/attack-`date +"%Y%m%d-%H%M%S"`.log
          echo "Packets Dump, Sleeping now"
-    sleep 120
+    sleep 360
  fi
 done
          
